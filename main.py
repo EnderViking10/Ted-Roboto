08047205cc40970d05755b4efea918f84dea2289
Erik Klem <mrerikk6@gmail.com>
2024-03-19 16:17:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import os
+import asyncio
 import random
 
 import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 
-async def ball8(message: discord.Message):
+@bot.hybrid_command(name="8ball", description="Shake the 8 ball")
+async def ball_8(ctx):
     responses = ['Outlook good', 'Better not tell you now', 'Most likely',
                  'Dont count on it', 'It is decidedly so',
                  'Yes, definitely', 'Concentrate and ask again',
@@ -12,95 +19,55 @@ async def ball8(message: discord.Message):
                  'try again', 'As I see it, yes',
                  'Failure is another form of Success', 'Yikes']
 
-    await message.reply(f'{random.choice(responses)}')
+    await ctx.send(f'{random.choice(responses)}')
 
 
-async def milestone(message: discord.Message):
+@bot.hybrid_command(name="milestone", description="You are actively taking a milestone exam.")
+async def milestone(ctx):
     response = 'Your environment is working as intended.'
 
-    await message.author.send(response)
+    await ctx.defer()
+    await ctx.message.author.send(response)
 
 
-async def feedback(message: discord.Message):
+@bot.hybrid_command(name="feedback", description="You are actively taking a module exam.")
+async def feedback(ctx):
     response = ("sparker told me that your last 4 premile attempts weren't a "
                 "100, you aren't ready for a feedback session.")
 
-    await message.author.send(response)
+    await ctx.defer()
+    await ctx.message.author.send(response)
 
 
-async def friday(message: discord.Message):
+@bot.hybrid_command(name="friday")
+async def friday(ctx):
     responses = ['https://youtu.be/kfVsfOSbJY0',
                  'https://youtu.be/1TewCPi92ro']
 
-    await message.reply(random.choice(responses))
+    await ctx.send(random.choice(responses))
 
 
-async def help_message(message: discord.Message):
-    response = ('#milestone: You are actively taking a milestone exam.\n'
-                '#exam: You are actively taking a module exam.\n'
-                '#feedback: You want to schedule a feedback session OR you '
-                'have finished an exam and want to ask questions about it.\n')
+@bot.hybrid_command(name="shame")
+async def shame(ctx, user: discord.User = None, message_number: int = 10):
+    if user is None:
+        return await ctx.message.reply('You must specify a user')
 
-    await message.reply(response)
-
-
-async def shame(message: discord.Message):
-    user = message.mentions
-    message_num = 10
-
-    message_args = message.content.split(' ')
-
-    if len(message.mentions) != 1:
-        await message.reply('You must mention a person')
-
-    if len(message_args) >= 3:
-        message_num = int(message_args[2])
-
-    for i in range(message_num):
-        await message.channel.send(f'{message.mentions[0].mention}: Log into chat!! https://tenor.com/view/cat-disapear-cat-snow-cat-jump-fail-cat-fun-jump-cats-gif-7283381407501919931')
+    await ctx.defer()
+    for i in range(message_number):
+        await ctx.message.channel.send(
+            f'{ctx.message.mentions[0].mention}: Log into chat!! https://tenor.com/view/cat-disapear-cat-snow-cat-jump-fail-cat-fun-jump-cats-gif-7283381407501919931')
 
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+@bot.command(name="sync", description="Sync Commands")
+async def sync(ctx):
+    synced = await ctx.bot.tree.sync()
 
-    async def on_message(self, message: discord.Message):
-        # Return if self
-        if message.author == self.user:
-            return
-        # Check for prefix
-        if message.content[0] != '#':
-            return
-
-        if '8ball' in message.content:
-            await ball8(message)
-
-        if 'milestone' in message.content or 'exam' in message.content:
-            await milestone(message)
-
-        if 'feedback' in message.content:
-            await feedback(message)
-
-        if 'friday' in message.content:
-            await friday(message)
-
-        if 'help' in message.content:
-            await help_message(message)
-
-        if 'shame' in message.content:
-            await shame(message)
-
-
-intents = discord.Intents.default()
-intents.message_content = True
+    await ctx.send(f"Synced {len(synced)} commands globally")
 
 
 def main():
-    TOKEN = ""
-
-    client = MyClient(intents=intents)
-    client.run(TOKEN)
+    bot.run('')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
